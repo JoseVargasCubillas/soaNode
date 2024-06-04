775c15820bc82c69427c959c60c5f0ee93dd9ea8
Jose Julian Vargas Cubillas <211100@ids.upchiapas.edu.mx>
2024-06-04 00:10:19 -0600

    terminado

---
+++ index.js
@@ -0,0 +1,1 @@
+const express = require('express'); const app = express(); const port = 3000;  app.get('/api/v1/healt', (req,res) =>{     res.send('hello'); });  app.listen(port, () => {     console.log('server is running'); });
